15a2f8311610f0c827e7a2db1801a53c83b2e67c
Meowcolm024 <luoyicong1026@live.com>
2018-03-10 21:13:47 +0800

    Add files via upload

---
+++ Swap.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int *left, int *right)
+{
+    int tmp;
+    tmp = *left;
+    *left = *right;
+    *right = tmp;
+};
+
+int main()
+{
+    int a = 5;
+    int b = 9;
+
+    swap(a,b);
+
+    cout << a << " " << b << endl;
+    return 0;
+}
