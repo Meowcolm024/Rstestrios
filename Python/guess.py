a21f8f4a33a2ba2c7066dba27f72433db2899be8
Meowcolm024 <luoyicong1026@live.com>
2019-07-28 11:10:42 +0800

    haha

---
+++ Python/guess.py
@@ -0,0 +1,24 @@
+# @Author: Meowcolm024
+
+from __future__ import print_function
+from random import randrange
+
+def main():
+    ans = randrange(0, 10)
+    t = 1
+    while t != 0:
+        x = input("guess a number beween 0 ad 10: ")
+        if not x.isdigit():
+            print("please input a number")
+            continue
+        t = ans - int(x)
+        if t < 0:
+            print("The number is smaller")
+        elif t > 0:
+            print("The number is larger")
+        else:
+            print("Correct!")    
+
+
+if __name__ == "__main__":
+    main()
